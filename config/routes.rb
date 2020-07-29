107b8cbd16d74cb3d745432272392e104ba138c3
vagrant <vagrant@localhost.localdomain>
2020-07-29 06:28:10 +0000

    [Add]login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/top'
+  get 'home/about'
+  root to: 'home#top'
   devise_for :users, controllers: {
   	sessions: 'users/sessions',
   	registrations: 'users/registrations'
 	}
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
