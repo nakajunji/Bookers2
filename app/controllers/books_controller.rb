7d9fe065176c1766b83bb3b707ec414d70e37649
vagrant <vagrant@localhost.localdomain>
2020-08-01 12:35:28 +0000

    [Add]edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,15 @@ class BooksController < ApplicationController
 	end
 
 	def create
+		@books = Book.all
+		@user = current_user
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-		@book.save
-		redirect_to book_path(@book.id)
+		if @book.save
+			redirect_to book_path(@book.id)
+		else
+			render "index"
+		end
 	end
 
 	def index
@@ -20,7 +25,19 @@ class BooksController < ApplicationController
 		@book = Book.new
 		@post_book = Book.find(params[:id])
 		@user = User.find(@post_book.user_id)
-		
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			redirect_to book_path(@book.id)
+		else
+			render "edit"
+		end
 	end
 
 	private
