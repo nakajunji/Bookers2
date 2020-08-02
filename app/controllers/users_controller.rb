aa0e6339443e3cad807ff1304b29122fa22fa48b
vagrant <vagrant@localhost.localdomain>
2020-08-02 09:57:21 +0000

    [Finish]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+
   def show
   	@user = User.find(params[:id])
     @book = Book.new
@@ -19,10 +22,18 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
   		redirect_to user_path(@user.id)
   	else
   		render "edit"
   	end
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :profile_image
-  validates :introduction, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { in: 2..20}
+  validates :introduction, length: { maximum: 50 }
   has_many :books, dependent: :destroy
 end
