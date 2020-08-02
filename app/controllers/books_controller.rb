7b58e21279284cb5140278849fb0d1fb207b29b2
vagrant <vagrant@localhost.localdomain>
2020-08-02 07:23:27 +0000

    [Add]Destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+	before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
 	def new
 		@book = Book.new
 	end
@@ -9,6 +10,7 @@ class BooksController < ApplicationController
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		if @book.save
+			flash[:notice] = "You have creatad book successfully."
 			redirect_to book_path(@book.id)
 		else
 			render "index"
@@ -34,9 +36,23 @@ class BooksController < ApplicationController
 	def update
 		@book = Book.find(params[:id])
 		if @book.update(book_params)
+			flash[:notice] = "You have updated book successfully."
 			redirect_to book_path(@book.id)
 		else
 			render "edit"
+		end
+	end
+
+	def destroy
+		@post_book = Book.find(params[:id])
+		@post_book.destroy
+		redirect_to books_path
+	end
+
+	def ensure_correct_user
+		@post_book = Book.find(params[:id])
+		if @post_book.user_id != current_user.id
+			redirect_to books_path
 		end
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   	registrations: 'users/registrations'
 	}
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
